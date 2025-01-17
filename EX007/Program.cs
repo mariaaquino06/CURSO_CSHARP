23f1c4ad3d11d2a46d0dbd05f845828a5f30b57f
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-16 21:53:28 -0300

    Atualização Exercicio

---
+++ EX007/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EX007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final;
+            string resultado;
+
+            Console.WriteLine("Digite a nota final do aluno");
+            nota_final = Convert.ToDouble(Console.ReadLine());
+
+            if (nota_final >= 60)
+            {
+                if (nota_final >= 90)
+                {
+                    resultado = "aprovado com mérito, parabéns!!";
+                }
+                else
+                {
+                    resultado = "aprovado!!";
+                }
+            }
+            else if (nota_final >= 40)
+            {
+                resultado = "de recuperação.";
+            }
+            else
+            {
+                if (nota_final >=40)
+                {
+                    resultado = "Recuperação";
+                }
+                else
+                {
+                    resultado = "Reprovado";
+                }
+
+            }
+
+            Console.WriteLine("Nota do Aluno: {0} - Resultado: {1}", nota_final, resultado);
+        }
+    }   
+       
+}
