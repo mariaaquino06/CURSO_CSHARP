81b45a6b271779ca7357965adecec674a2d97a5e
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-21 19:32:40 -0300

    Exercício Triângulos

---
+++ Par ou Ímpar/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Par_ou_Ímpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um Número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0) 
+
+
+        }
+    }
+}
---
+++ TESTE__TRIANGULO/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TESTE__TRIANGULO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.WriteLine("Teste de Triângulos");
+            Console.WriteLine("===================");
+
+            Console.WriteLine("Digite o Valor do Lado A");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o Valor do Lado B");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o Valor do Lado C");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("==========================================");               
+                Console.WriteLine("Os valores informados formam um Triângulo");
+                Console.WriteLine("==========================================");
+            } 
+            else 
+            {
+                Console.WriteLine("==============================================");
+                Console.WriteLine("Os valores informados, NÃO formam um Triângulo");
+                Console.WriteLine("==============================================");
+            }
+              
+            
+
+
+             
+        }
+    }
+}
