9ea95fe92aaefb7758f592d07938880356b00524
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-21 21:14:53 -0300

    teste 1 salario

---
+++ TESTE_SALARIO/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TESTE_SALARIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            Console.WriteLine("");
+            Console.WriteLine("Insira o Valor do seu Salário");
+            
+            salario = Convert.ToDouble(Console.ReadLine());
+         
+            
+            if (salario > 2500)
+            {
+                salario += (0.05 * salario);
+                Console.WriteLine("O Aumento de salário é de R$");
+            }
+            else if (salario > 1250)
+            {
+
+               
+            }
+        }
+
+
+
+    }
+}
