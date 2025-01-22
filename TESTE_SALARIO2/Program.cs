cfb0e933a1e397c2b3ae8e98a4f3f794c618a97b
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-21 22:00:32 -0300

    Todos os Salários certos

---
+++ TESTE_SALARIO2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TESTE_SALARIO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioAtual;
+            double salarioAumento; 
+            Console.WriteLine("Insira seu Salário Atual R$");
+            salarioAtual = Convert.ToDouble(Console.ReadLine());
+
+            if (salarioAtual > 2500)
+            {
+                salarioAumento = (0.05 * salarioAtual) + salarioAtual;
+                Console.WriteLine("O Novo salario é igual a {0}.", salarioAumento);  
+
+            }
+            else if (salarioAtual > 1250)
+            {
+                salarioAumento = (0.10 * salarioAtual) + salarioAtual;
+                Console.WriteLine("O novo salario é gual a {0}.", salarioAumento);
+            }
+            else
+            {
+                salarioAumento = (0.15 * salarioAtual) + salarioAtual;
+                Console.WriteLine("O novo salario é igual a {0}.", salarioAumento);
+            }
+           
+        }
+    }
+}
