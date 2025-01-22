6d438092396f4853af88ca9189a3296b1c8daebd
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-21 22:18:42 -0300

    EMPRESTIMO CERTO

---
+++ TESTE_BANCO2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace TESTE_BANCO2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorCasa, salario, prestacaoMensal;
+            int anosEmprestimo, mesesEmprestimo;
+
+            Console.WriteLine("Digite o Valor da casa:");
+            valorCasa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite seu Salario:");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Quantos anos de emprestimo:");
+            anosEmprestimo = Convert.ToInt32(Console.ReadLine());
+
+            mesesEmprestimo = anosEmprestimo * 12;
+
+            prestacaoMensal = valorCasa / mesesEmprestimo; 
+
+            if (prestacaoMensal < salario * 0.30)
+            {
+                Console.WriteLine("Emprestimo Aceito");
+                Console.WriteLine("Parcelas: {0}", mesesEmprestimo);
+                Console.WriteLine("Valor: {0} mensais", prestacaoMensal);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Emprestimo Negado");
+                Console.WriteLine("O valor das prestações ultrapassam 30% do seu salário.");
+
+            }
+        }
+    }
+}
