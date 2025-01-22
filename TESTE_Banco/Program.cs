438fa94c1d1b8f3c8477794fedade86d62e3d602
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-21 22:09:31 -0300

    TESTE 1

---
+++ TESTE_Banco/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TESTE_Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorCasa;
+            int salarioComprador;
+            int anos;
+
+            Console.WriteLine("===============================");
+            Console.WriteLine("   Simulação de Emprestimo");
+            Console.WriteLine("===============================");
+            Console.WriteLine();
+            Console.WriteLine("Insira o Valor da Casa desejada:");
+            valorCasa = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
