3e919018b2bf1b55614a5cf258f53484522d2285
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-28 19:45:30 -0300

    SENHA CORRETA E INCORRETA

---
+++ EX SENHA/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EX_SENHA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123"; // Senha correta 
+            string senhaDigitada; // Senha digitada pelo usuário
+            int tentativas = 0; // Contador de Tentativas
+            
+            // Loop para solicitar a senha do usuário
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a Senha");
+                
+                senhaDigitada = Console.ReadLine();
+                tentativas++; // Incrementa o contador de tentativas
+                
+                // verifica se a senha digitada é diferente da senha correta
+                if (senhaDigitada != senha && tentativas < 3)
+                {
+                 Console.WriteLine("Senha Incorreta");
+                 Console.WriteLine("Você ainda tem " + ( 3 - tentativas) +  " Tentativa(s)");
+                 Console.ReadKey();
+                } 
+                else if (senhaDigitada != senha && tentativas == 3)
+                {
+                 return;
+                }
+                
+            } while (senhaDigitada != senha);
+
+            Console.Clear ();
+            Console.WriteLine("Senha Correta - Tentativas:" + tentativas); 
+
+        }
+    }
+}
---
+++ EX030/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EX030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUMERO = 3; 
+
+            while (NUMERO < 5)
+            {
+                NUMERO++;
+                Console.WriteLine("Curso C#");
+
+            }
+
+            do
+            {
+                NUMERO++;
+                Console.WriteLine("Curso C#");
+
+            } while (NUMERO < 5);
+        }
+    }
+}
