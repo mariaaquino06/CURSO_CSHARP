1f93ee5da8c9b2f25fd338917908b8d91de0b781
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-28 22:23:57 -0300

    frutas

---
+++ EX_FRUTAS/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EX_FRUTAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas ={"Tomate", "morango", "Limão", "Laranja", "Pessego", "Melancia", "Uva" };
+            double[] valores = { 10, 12, 7, 14, 18, 11, 15 };
+
+            for (int i = 0; i < frutas.Length; i++)
+            {
+             Console.WriteLine("{0} ---> R$ {1}", frutas[i], valores[i]);
+             
+            }
+            Console.WriteLine("A fruta com maior preço é:");
+            Console.WriteLine(valores.Max());
+        }
+    }
+}
