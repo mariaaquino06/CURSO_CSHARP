b88c9787a7ef6299edc89b806b60e00b3809ed68
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-28 21:56:34 -0300

    meses

---
+++ EX_DATAS/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EX_DATAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = {"Janeiro","Fevereiro","Março","Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro" };
+            int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; 
+            for (int i = 0; i < meses.Length; i++)
+            {
+                Console.WriteLine("O mês de " + meses[i] + "tem" + dias[i] + "dias.");
+
+            }
+            string[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+
+            
+        }
+    }
+}
