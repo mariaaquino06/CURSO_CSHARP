3c58e258beb6207abcc271432bd1fdcabde0ec2f
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-28 22:15:40 -0300

    MAIOR E MENOR

--- EX_DATAS/Program.cs
+++ EX_DATAS/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             string[] meses = {"Janeiro","Fevereiro","Março","Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
                 "Outubro", "Novembro", "Dezembro" };
             int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; 
@@ -12,9 +13,20 @@
                 Console.WriteLine("O mês de " + meses[i] + "tem" + dias[i] + "dias.");
 
             }
+            Console.WriteLine();
             string[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
 
+            foreach(string dia in semana)
+            {
+             Console.WriteLine(dia);
 
+            }*/
+
+            int[] numeros = { 10, 45, 22, 50, 6, -84, -2, 91, -23 };
+            Console.WriteLine(numeros.First());
+            Console.WriteLine(numeros.Last());
+            Console.WriteLine(numeros.Max());
+            Console.WriteLine(numeros.Min());
             
         }
     }
