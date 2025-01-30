776d9d8c3d738c41ed329fe4d54190f3d89e1b48
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-30 20:53:28 -0300

    metodos

---
+++ METODOS/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace METODOS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Saudacao("Maria Eduarda");
+            Soma(50, 63);
+         
+        }
+        static void Saudacao(string nome)
+        {
+            Console.WriteLine($"Olá {nome}, Seja bem-vindo(a)!");
+
+        }
+        static void Soma(int a, int b)
+        {
+         int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+        }
+    }
+}
