9670461b701c271ea42d78d096c71ddfdbaf1e8e
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-30 21:09:57 -0300

    area terreno

---
+++ CALCULO_AREA/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CALCULO_AREA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" =====================");
+            Console.WriteLine(" Controle de Terrenos");
+            Console.WriteLine(" =====================");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Digite a Largura do Terreno (m): ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o comprimento do Terreno (m): ");
+            double comprimento = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaTerreno(largura, comprimento);
+        }
+        static void AreaTerreno(double L, double c)
+        {
+            double a = L * c;
+            Console.WriteLine($" A área do terreno de {L}m * {c}m é de {a:F2}m².");
+
+        }
+
+    }
+}
