9f4aecd1bb8cf43d1f8ba2ae374a449c08c294c7
Maria Eduarda <maria.eoaquino@senacsp.edu.br>
2025-01-30 21:49:50 -0300

    centralizado

---
+++ ESCREVER/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ESCREVER
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('*', tam);
+            string TextoCentralizado= texto.PadLeft(tam - 2).PadRight(tam-2);
+            Console.WriteLine(linha);
+            Console.WriteLine(TextoCentralizado);
+            Console.WriteLine(linha);
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Escrever("Curso C#");
+            Console.WriteLine();
+            Escrever(" Maria Eduarda Oliveira Aquino");
+            Console.WriteLine();
+            Escrever("Olá");
+            Console.WriteLine(); 
+        }
+    }
+}
